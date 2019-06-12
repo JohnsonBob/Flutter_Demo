7a97b37232832ca843a9bcaf13ba68b78f5e8591
JohnsonMac <1306305902@qq.com>
2019-06-12 23:47:00 +0800

    1、add PageViewBuliderDemo

--- lib/demo/view_demo.dart
+++ lib/demo/view_demo.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
+import '../model/post.dart';
 
 class ViewDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return PageViewDemo();
+    return PageViewBuliderDemo();
   }
 
 
 }
 
-class PageViewDemo extends StatelessWidget{
+class PageViewDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -20,7 +21,7 @@ class PageViewDemo extends StatelessWidget{
 //    reverse: true,
       //设置滚动方向
 //      scrollDirection: Axis.vertical,
-      onPageChanged: (currentPage)=> debugPrint('Page: $currentPage'),
+      onPageChanged: (currentPage) => debugPrint('Page: $currentPage'),
       controller: PageController(
         //初始要显示的界面
           initialPage: 1,
@@ -35,7 +36,7 @@ class PageViewDemo extends StatelessWidget{
           alignment: Alignment(0, 0),
           child: Text(
             "ONE",
-            style:TextStyle(fontSize: 32.0, color: Colors.white),
+            style: TextStyle(fontSize: 32.0, color: Colors.white),
           ),
         ),
         Container(
@@ -43,7 +44,7 @@ class PageViewDemo extends StatelessWidget{
           alignment: Alignment(0, 0),
           child: Text(
             "TWO",
-            style:TextStyle(fontSize: 32.0, color: Colors.white),
+            style: TextStyle(fontSize: 32.0, color: Colors.white),
           ),
         ),
         Container(
@@ -51,12 +52,52 @@ class PageViewDemo extends StatelessWidget{
           alignment: Alignment(0, 0),
           child: Text(
             "THREE",
-            style:TextStyle(fontSize: 32.0, color: Colors.white),
+            style: TextStyle(fontSize: 32.0, color: Colors.white),
           ),
         ),
       ],
     );
   }
 
+}
+
+class PageViewBuliderDemo extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return PageView.builder(
+        itemCount: posts.length,
+        itemBuilder: _pageItemBulider
+    );
+  }
+
+  Widget _pageItemBulider(BuildContext context, int index) {
+    return Stack(
+      children: <Widget>[
+        SizedBox.expand(
+          child: Image.network(
+            posts[index].imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Positioned(
+          bottom: 8.0,
+          left: 8.0,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                posts[index].title,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(
+                  posts[index].author
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
 }
 
